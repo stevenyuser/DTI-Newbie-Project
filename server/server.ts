6b10e6d1428d8ebda76c366de8b644f74a97600c
Jasmine Li <jasminexli@126.com>
2023-11-19 23:38:33 -0500

    fixed gitignore file added firebase config

--- server/server.ts
+++ server/server.ts
@@ -7,12 +7,16 @@ const port = 8080
 app.use(express.json())
 app.use(cors())
 
-// route
+// sample route
 app.get("/", async (req, res) => {
     res.status(200).json({message: "hello"})
 })
 
 
+// routes
+
+
+// listening on port
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`)
 })

8b24b2f995e51755ecc66fcdd3f33c9339f00f21
Steven Yu <stevenyuser@gmail.com>
2023-12-01 17:26:32 -0500

    added metadata for companies/[id]

--- frontend/src/app/companies/[id]/page.tsx
+++ frontend/src/app/companies/[id]/page.tsx
@@ -1,14 +1,27 @@
+import { Metadata } from "next"
+
+const validCompanies = ["OurBus", "C2C", "FlixBus", "MegaBus"];
+
+export async function generateMetadata({ params }: any) {
+    const metadata: Metadata = {
+        title: `CUSoon - ${validCompanies.includes(params.id as string) ? params.id : "Invalid Company"}`,
+        description: 'Connecting Cornell to the world 🌎',
+    }
+
+    return metadata;
+}
+
 export default function CompanyPage({ params }: any) {
-    const validUrls = ["OurBus", "C2C", "FlixBus"];
-    // params.id
     
     return (
-        <div>
-            {!validUrls.includes(params.id as string) && 
-                <div>
-                    <h1>Invalid URL</h1>
-                </div>
-            }
-        </div>
+        <main>
+            <div>
+                {!validCompanies.includes(params.id as string) && 
+                    <div>
+                        <h1>Invalid URL</h1>
+                    </div>
+                }
+            </div>
+        </main>
     )
 }

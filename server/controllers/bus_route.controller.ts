6ec93ef6359df2facf7742067754156c8fa8cc3f
Steven Yu <stevenyuser@gmail.com>
2023-12-02 01:32:05 -0500

    updated api to include megabus

--- server/controllers/bus_route.controller.ts
+++ server/controllers/bus_route.controller.ts
@@ -1,8 +1,8 @@
-import { GeneralLocations, C2CLocations, OurBusLocations, BusRoute } from "../../common/types";
-import { scrapeC2C, scrapeOurBus } from "../services/scraping.service";
+import { GeneralLocations, C2CLocations, OurBusLocations, BusRoute, MegabusLocations } from "../../common/types";
+import { scrapeC2C, scrapeMegabus, scrapeOurBus } from "../services/scraping.service";
 
 export const getBusRoutes = async (origin: GeneralLocations, destination: GeneralLocations, date: Date) => {
-    let ourbusData, c2cData: BusRoute[];
+    let ourbusData, c2cData, megabusData: BusRoute[];
 
     if(origin === GeneralLocations.Ithaca as GeneralLocations && destination === GeneralLocations.NYC) { // ITH -> NYC
         let [ourbusDataFortLee, ourbusDataNYC] = await Promise.all([
@@ -12,6 +12,9 @@ export const getBusRoutes = async (origin: GeneralLocations, destination: Genera
         ourbusData = ourbusDataFortLee.concat(ourbusDataNYC);
         
         c2cData = await scrapeC2C(C2CLocations.IthacaNorthCampus, C2CLocations.NYCCornellClub, date);
+
+        megabusData = await scrapeMegabus(MegabusLocations.Ithaca, MegabusLocations.NYC, date);
+
     } else { // NYC -> ITH
         let [ourbusDataFortLee, ourbusDataNYC] = await Promise.all([
             scrapeOurBus(OurBusLocations.FortLee, OurBusLocations.Ithaca, date),
@@ -20,9 +23,12 @@ export const getBusRoutes = async (origin: GeneralLocations, destination: Genera
         ourbusData = ourbusDataFortLee.concat(ourbusDataNYC);
         
         c2cData = await scrapeC2C(C2CLocations.NYCCornellClub, C2CLocations.IthacaNorthCampus, date);
+
+        megabusData = await scrapeMegabus(MegabusLocations.NYC, MegabusLocations.Ithaca, date);
+
     }
 
-    const busRoutes: BusRoute[] = ourbusData.concat(c2cData);
+    const busRoutes: BusRoute[] = ourbusData.concat(c2cData).concat(megabusData);
 
     if (busRoutes === undefined) {
         return [];

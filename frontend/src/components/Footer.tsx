dcf56e1bc6868a19da3763ad2eed2439c7b6f145
Steven Yu <stevenyuser@gmail.com>
2023-12-02 06:32:55 -0500

    fixed /api/reviews/create endpoint

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+    <footer
+        className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+        <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+            Made with ❤️ and 🧠 by @stevenyu and @jasminexli
+            <a
+                className="text-neutral-800 dark:text-neutral-400"
+                href="https://tw-elements.com/"
+            >TW elements</a>
+        </div>
+    </footer>
+}
+
--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ app.use(cors())
 
 // sample route
 app.get("/", async (req, res) => {
-    res.status(200).json({message: "hello"})
+    res.status(200).json({ message: "hello" })
 })
 
 // bus_route routes
@@ -30,12 +30,12 @@ app.post("/api/routes", async (req, res) => {
         const busRoutes = await getBusRoutes(origin, destination, date);
         console.log(date);
         res.status(200).send({
-          message: `SUCCESS retrieved bus routes for ${origin} to ${destination} on ${date}`,
-          data: busRoutes,
+            message: `SUCCESS retrieved bus routes for ${origin} to ${destination} on ${date}`,
+            data: busRoutes,
         });
     } catch (err) {
         res.status(500).json({
-          error: `ERROR: an error occurred in the /api/routes endpoint: ${err}`,
+            error: `ERROR: an error occurred in the /api/routes endpoint: ${err}`,
         });
     }
 })
@@ -46,12 +46,12 @@ app.get("/api/companies", async (req, res) => {
     try {
         const companies = await getAllCompanies();
         res.status(200).send({
-          message: `SUCCESS retrieved ${companies} from the bus_companies collection in Firestore`,
-          data: companies,
+            message: `SUCCESS retrieved ${companies} from the bus_companies collection in Firestore`,
+            data: companies,
         });
     } catch (err) {
         res.status(500).json({
-          error: `ERROR: an error occurred in the /api/companies endpoint: ${err}`,
+            error: `ERROR: an error occurred in the /api/companies endpoint: ${err}`,
         });
     }
 });
@@ -64,19 +64,19 @@ app.get("/api/companies/:companyId", async (req, res) => {
 
         if (company === null) {
             res
-            .status(404)
-            .send({
-              error: `ERROR: company with companyId: ${companyId} not found in Firestore`,
-            });
+                .status(404)
+                .send({
+                    error: `ERROR: company with companyId: ${companyId} not found in Firestore`,
+                });
         } else {
             res.status(200).send({
-              message: `SUCCESS retrieved company with companyId: ${companyId} from the companies collection in Firestore`,
-              data: company,
+                message: `SUCCESS retrieved company with companyId: ${companyId} from the companies collection in Firestore`,
+                data: company,
             });
         }
     } catch (err) {
         res.status(500).json({
-          error: `ERROR: an error occurred in the /api/companies/:companyId endpoint: ${err}`,
+            error: `ERROR: an error occurred in the /api/companies/:companyId endpoint: ${err}`,
         });
     }
 });
@@ -90,19 +90,19 @@ app.get("/api/companies/:companyId/averageRating", async (req, res) => {
 
         if (averageRating === null) {
             res
-            .status(404)
-            .send({
-              error: `ERROR: average rating of company with companyName: ${companyId} not found in Firestore`,
-            });
+                .status(404)
+                .send({
+                    error: `ERROR: average rating of company with companyName: ${companyId} not found in Firestore`,
+                });
         } else {
             res.status(200).send({
-              message: `SUCCESS retrieved average rating of company with companyName: ${companyId} from the companies collection in Firestore`,
-              data: averageRating,
+                message: `SUCCESS retrieved average rating of company with companyName: ${companyId} from the companies collection in Firestore`,
+                data: averageRating,
             });
         }
     } catch (err) {
         res.status(500).json({
-          error: `ERROR: an error occurred in the /api/companies/:company/averageRating endpoint: ${err}`,
+            error: `ERROR: an error occurred in the /api/companies/:company/averageRating endpoint: ${err}`,
         });
     }
 })
@@ -112,34 +112,32 @@ app.get("/api/companies/:companyId/averageRating", async (req, res) => {
 // add review
 app.post("/api/reviews/create", async (req, res) => {
     console.log("[POST] entering '/reviews/create' endpoint");
-        const { busCompanyId, rideDate, ridePrice, rideOrigin, rideDestination, title, rating, reviewText,
-            likes, userName, netId, reviewDateTime } = req.body;
-        // rideDate is in "YYYY-MM-DD" format
-        // dateTimeOfPosting is in standard ISO format: "yyyy-MM-dd'T;HH:mm:ss"
-        
-        // console.log(rideDate);
-        const review: Review = {
-            busCompanyId,
-            rideDate,
-            ridePrice,
-            rideOrigin,
-            rideDestination,
-            title,
-            rating,
-            reviewText,
-            userName,
-            reviewDateTime
+    // rideDate is in "YYYY-MM-DD" format
+    // dateTimeOfPosting is in standard ISO format: "yyyy-MM-dd'T'HH:mm:ss"
+
+    // console.log(rideDate);
+    const review: Review = {
+        "busCompanyId": req.body.busCompanyId,
+        "rideDate": req.body.rideDate,
+        "ridePrice": req.body.ridePrice,
+        "rideOrigin": req.body.rideOrigin,
+        "rideDestination": req.body.rideDestination,
+        "title": req.body.title,
+        "rating": req.body.rating,
+        "reviewText": req.body.reviewText,
+        "userName": req.body.userName,
+        "reviewDateTime": req.body.reviewDateTime,
     };
 
     try {
         await addReview(review);
         res.status(200).send({
-            message: `SUCCESS added review from ${userName} to the reviews collection in Firestore`,
+            message: `SUCCESS added review from ${req.body.userName} to the reviews collection in Firestore`,
         });
     } catch (err) {
         res.status(500).json({
-            error: `ERROR: an error occurred in the /api/reviews/create endpoint: ${err}`,   
-    }) 
+            error: `ERROR: an error occurred in the /api/reviews/create endpoint: ${err}`,
+        })
     }
 }
 )
@@ -154,38 +152,38 @@ app.get("/api/reviews/:companyId/all", async (req, res) => {
 
         if (company === null) {
             res
-            .status(404)
-            .send({
-              error: `ERROR: company with companyName: ${companyId} not found in Firestore`,
-            });
+                .status(404)
+                .send({
+                    error: `ERROR: company with companyName: ${companyId} not found in Firestore`,
+                });
         } else {
             let reviews = await getReviewsByCompany(companyId);
             res.status(200).send({
-              message: `SUCCESS retrieved reviews associated wiht companyName: ${companyId} from the reviews collection in Firestore`,
-              data: reviews, 
+                message: `SUCCESS retrieved reviews associated wiht companyName: ${companyId} from the reviews collection in Firestore`,
+                data: reviews,
             });
             console.log(reviews);
         }
     } catch (err) {
         res.status(500).json({
-          error: `ERROR: an error occurred in the /api/companies/:company endpoint: ${err}`,
+            error: `ERROR: an error occurred in the /api/companies/:company endpoint: ${err}`,
         });
     }
-}); 
+});
 
 // delete specific review
 app.delete("/api/reviews/:review_id/delete", async (req, res) => {
-    
+
     const reviewId: string = req.params.review_id;
-    
+
     try {
         await deleteReview(reviewId);
         res.status(200).send({
             message: `SUCCESS deleted review with : ${reviewId} from the reviews collection in Firestore`,
-          });
+        });
     } catch (err) {
         res.status(500).json({
-          error: `ERROR: an error occurred in the /api/reviews/:review_id/delete endpoint: ${err}`,
+            error: `ERROR: an error occurred in the /api/reviews/:review_id/delete endpoint: ${err}`,
         });
     }
 });

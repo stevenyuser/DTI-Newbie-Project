976cee2d625f0e27579c2699924345b211642424
Steven Yu <stevenyuser@gmail.com>
2023-12-02 00:53:18 -0500

    added retrieving bus company date to /companies/[id]

--- frontend/src/app/companies/[id]/page.tsx
+++ frontend/src/app/companies/[id]/page.tsx
@@ -1,27 +1,75 @@
+'use client'
+
 import { Metadata } from "next"
+import BusCompanyHeader from "@/components/BusCompanyHeader";
+import BusCompanyHighlights from "@/components/BusCompanyHighlights";
+import { useEffect, useState } from "react";
+import { BusCompany } from "../../../../../common/types";
 
-const validCompanies = ["OurBus", "C2C", "FlixBus", "MegaBus"];
+const validCompanies = ["OurBus", "C2C", "Flixbus", "Megabus"];
 
-export async function generateMetadata({ params }: any) {
-    const metadata: Metadata = {
-        title: `CUSoon - ${validCompanies.includes(params.id as string) ? params.id : "Invalid Company"}`,
-        description: 'Connecting Cornell to the world 🌎',
+// export async function generateMetadata({ params }: any) {
+//     const metadata: Metadata = {
+//         title: `CUSoon - ${validCompanies.includes(params.id as string) ? params.id : "Invalid Company"}`,
+//         description: 'Connecting Cornell to the world 🌎',
+//     }
+
+//     return metadata;
+// }
+
+async function getBusCompanyData(companyId: string): Promise<BusCompany | null> {
+    const res = await fetch(`http://0.0.0.0:8080/api/companies/${companyId}`);    
+    const data = await res.json();
+
+    const busCompany: BusCompany = data.data as BusCompany;
+
+    if (busCompany === undefined) {
+        return null;
     }
 
-    return metadata;
+    console.log("bus company: " + JSON.stringify(busCompany));
+    return busCompany;
 }
 
 export default function CompanyPage({ params }: any) {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [busCompany, setBusCompany] = useState<BusCompany | null>(null);
     
+    useEffect(() => {
+        getBusCompanyData(params.id)
+            .then((returnedBusCompany) => setBusCompany(returnedBusCompany))
+            .then(() => setIsLoading(false));
+    }, [])
+
     return (
         <main>
-            <div>
-                {!validCompanies.includes(params.id as string) && 
-                    <div>
-                        <h1>Invalid URL</h1>
-                    </div>
-                }
-            </div>
+            {!validCompanies.includes(params.id as string) &&
+                <div>
+                    <h1>Invalid URL</h1>
+                </div>
+            }
+
+            {validCompanies.includes(params.id as string) && isLoading &&
+                <div>
+                    <h1>Loading...</h1>
+                </div>
+            }
+
+            {validCompanies.includes(params.id as string) && !isLoading && (busCompany===null || busCompany === undefined) &&
+                <div>
+                    <h1>Error</h1>
+                </div>
+            }
+
+            {validCompanies.includes(params.id as string) && !isLoading && !(busCompany===null || busCompany === undefined) &&
+                <div className="">
+                    <BusCompanyHeader company={busCompany as BusCompany} />
+                    <BusCompanyHighlights company={busCompany as BusCompany} />
+                </div>
+            }
+
         </main>
     )
 }
+
+

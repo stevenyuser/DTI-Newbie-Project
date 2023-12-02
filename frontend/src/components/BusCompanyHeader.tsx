97f4db202109b2937918180518eab45cc520f021
Steven Yu <stevenyuser@gmail.com>
2023-12-02 02:50:16 -0500

    updated bus company header styling

--- frontend/src/components/BusCompanyHeader.tsx
+++ frontend/src/components/BusCompanyHeader.tsx
@@ -24,18 +24,23 @@ export default function BusCompanyHeader({ company }: BusCompanyHeaderProps) {
 
         <div className="flex flex-row justify-between space-x-10">
 
-          <div>
+          <div className="space-y-2">
             <h1 className="text-8xl font-extrabold tracking-tight text-white sm:text-5xl">{company.name}</h1>
 
-            <p className="text-md text-indigo-100 max-w-3xl">
+            <p className="text-xl text-white max-w-3xl font-medium">
               {company.description}
             </p>
 
+          <div className="flex flex-row items-center space-x-1 text-white text-lg">
             <Rating name="read-only" value={company.averageRating} readOnly precision={0.5} size="large"/>
+            <p className="font-medium">({Number(company.averageRating).toFixed(1)})</p>
+            <p>{company.numReviews} reviews</p>
+          </div>
+
           </div>
 
 
-          <div className="pt-12">
+          <div className="pt-16">
             <a href={company.websiteUrl} target="_blank" className="h-12 inline-flex items-center px-6 py-3 border border-white shadow-sm text-base font-medium rounded-md text-white opacity-100 hover:bg-red-300 hover:bg-opacity-60 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               Visit Site
               <ArrowTopRightOnSquareIcon className="ml-3 -mr-1 h-5 w-5" />

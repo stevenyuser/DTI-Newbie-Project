ea52adfeccc0e332e7adbc8a9d2bf34e72b580be
Steven Yu <stevenyuser@gmail.com>
2024-01-10 23:13:31 -0500

    added env

--- server/firebase.ts
+++ server/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import serviceAccount from "./service_account.json";
 
+// vercel env 
 const app = initializeApp({
   credential: cert(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string),
 });
